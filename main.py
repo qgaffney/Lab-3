0000e95d050021f79a94661ede6dd954fa6b51f6
qgaffney <90237467+qgaffney@users.noreply.github.com>
2024-10-01 16:54:42 -0400

    save

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+#Import OS module
+import os
+
+#Import module to read CSV
 import csv
 
-with open('Resource'/'budget_data (1).csv', mode='r') as csv.file:
-    csvFile = csv.reader(csv.file)
-    for lines in csvFile:
-        print(lines)
+#Set path for source file
+CSV_PATH = os.path.join ('Resource', 'budget_data (1).csv')
+
+#Open and read CSV
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+data=[]
+with open(CSV_PATH) as csvfile:
+
+#Specify delimiter and CSV reader
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+#Read header row, store it
+    csv_header = next(csvreader)
+
